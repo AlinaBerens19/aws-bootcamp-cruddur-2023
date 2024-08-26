88152763d8e2e8f7591771553a193aa94c188eb8
Alina Berens <berens.alina019@gmail.com>
2024-08-26 13:27:49 +0000

    Updates of docker

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -5,7 +5,7 @@ class HomeActivities:
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
-      'message': 'Cloud is fun!',
+      'message': 'Avidan & Alina!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
@@ -34,7 +34,7 @@ class HomeActivities:
     {
       'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
       'handle':  'Garek',
-      'message': 'My dear doctor, I am just simple tailor',
+      'message': 'My dear doctor, I am just simple tailor!',
       'created_at': (now - timedelta(hours=1)).isoformat(),
       'expires_at': (now + timedelta(hours=12)).isoformat(),
       'likes': 0,
