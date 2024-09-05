bb9b9cab435570e1a9f276ae953965e0c82e7520
Alina Berens <berens.alina019@gmail.com>
2024-09-05 09:35:51 +0000

    notification frontent fixes

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+import NotificationsFeedPage from './pages/NotificationsFeedPage';
 import HomeFeedPage from './pages/HomeFeedPage';
 import UserFeedPage from './pages/UserFeedPage';
 import SignupPage from './pages/SignupPage';
@@ -9,7 +10,6 @@ import MessageGroupsPage from './pages/MessageGroupsPage';
 import MessageGroupPage from './pages/MessageGroupPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
-import process from 'process';
 import {
   createBrowserRouter,
   RouterProvider
@@ -19,6 +19,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeFeedPage />
+  },
+  {
+    path: "/notifications",
+    element: <NotificationsFeedPage />
   },
   {
     path: "/@:handle",
